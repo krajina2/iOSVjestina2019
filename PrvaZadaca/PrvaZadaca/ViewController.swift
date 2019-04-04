21d7987607d7d7e05de1cc1bd996924f40143608
Marko Krajina <krajina2@gmail.com>
2019-04-04 18:06:35 +0200

    Pocetni projekt za prvu zadacu.

---
+++ PrvaZadaca/PrvaZadaca/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrvaZadaca
+//
+//  Created by Marko Krajina on 04/04/2019.
+//  Copyright © 2019 Marko Krajina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
